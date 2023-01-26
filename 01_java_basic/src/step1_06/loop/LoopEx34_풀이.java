0dce91aadc9681094c36eb7ea58277401276c824
12_web_LTH <12_web_LTH@DESKTOP-P7C48M6>
2023-01-26 14:29:55 +0900

    commit test

--- 01_java_basic/src/step1_06/loop/LoopEx34_풀이.java
+++ 01_java_basic/src/step1_06/loop/LoopEx34_풀이.java
@@ -3,5 +3,9 @@ package step1_06.loop;
 public class LoopEx34_풀이 {
 	public static void main(String[] args) {
 		System.out.println("solved");
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("hi");
+		}
 	}
 }
