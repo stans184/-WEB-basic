75fd482a1fb355181218846a0b699ad1bc69dd95
12_web_LTH <12_web_LTH@DESKTOP-P7C48M6>
2023-02-01 12:02:47 +0900

    account problem solve

--- 02_java_array_basic/src/step2_01/array/ArrayEx23_문제.java
+++ 02_java_array_basic/src/step2_01/array/ArrayEx23_문제.java
@@ -1,5 +1,6 @@
 package step2_01.array;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 /*
@@ -34,11 +35,68 @@ public class ArrayEx23_문제 {
 			System.out.print("메뉴 선택 : ");
 			selectMenu = sc.nextInt();
 			
-			if		(selectMenu == 1) {}
-			else if (selectMenu == 2) {}
-			else if (selectMenu == 3) {}
+			if		(selectMenu == 1) {
+				System.out.print("Enter Account Number : ");
+				int inAcc = sc.nextInt();
+				boolean accCheck = true;
+				
+				for (int i = 0; i < accs.length; i++) {
+					if (inAcc == accs[i]) {
+						accCheck = false;
+						System.out.println("Already has same Account");
+					}
+				}
+				
+				if (accCheck) {
+					if (accsCnt == 5) {
+						System.out.println("Account memory full, please delete");
+					}else {
+						accs[accsCnt] = inAcc;
+						accsCnt += 1;
+						
+						System.out.print("Enter Account PW : ");
+						int inPw = sc.nextInt();
+						pws[accsCnt] = inPw;
+					}
+				}
+				System.out.print("Account List : ");
+				System.out.println(Arrays.toString(accs));
+			}
+			else if (selectMenu == 2) {
+				System.out.print("Enter Account Number : ");
+				int inAcc = sc.nextInt();
+				int checkIdx = 0;
+				
+				for (int i = 0; i < accs.length; i++) {
+					if (inAcc == accs[i]) checkIdx = i;
+				}
+				
+				System.out.print("Enter Account PW : ");
+				int inPw = sc.nextInt();
+				
+				if (inPw == pws[checkIdx]) {
+					System.out.println("Account WithDrawal");
+					
+					for (int i = checkIdx; i < accs.length-1; i++) {
+						accs[i] = accs[i+1];
+					}
+					for (int i = checkIdx; i < pws.length-1; i++) {
+						pws[i] = pws[i+1];
+					}
+					accs[accs.length-1] = 0;
+					pws[pws.length-1] = 0;
+					accsCnt -= 1;
+				}else System.out.println("Wrong PW");
+				
+				System.out.print("Account List : ");
+				System.out.println(Arrays.toString(accs));
+			}
+			else if (selectMenu == 3) {
+				System.out.println("Program Exit");
+				isRun = false;
+			}
 			
 		}
-
+		sc.close();
 	}
 }
